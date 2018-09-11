b07efe4ec70ca1f6eb97d4b2878c64bbdd97649e
Vasja Markič <42490634+vasjamarkic@users.noreply.github.com>
2018-09-11 23:27:13 +0200

    Fix the code for the control unit usingROBOTDYN MEGA PRO CH340G /Atmega2560
    Added:
    - 3 PWM outputs for driving the dashboard lights, voltage and temperature gauges
    - 3 analog inputs for measuring dim, voltage of the battery pack (via resistor divider) and temperature.
    Changed:
    - cabin lights will be drived directly! (not via central unit)
    - added the rear wiper function

--- krmilje_sekundarni_krog_VW_T3_MEGA_INVERT_ver2_NEXTION.ino
+++ krmilje_sekundarni_krog_VW_T3_MEGA_INVERT_ver2_NEXTION.ino
@@ -1,89 +1,81 @@
-/*TRUE last version for NEXTION display with ARDUINO mega2560, connection working 
-26.6.2018 (Ivan's Arduino MEGA 2560 :)
+/*FIRST version NEXTION display with ARDUINO mega2560 PRO, connection working 
 linked with HMI file "NEXTION_2_4_ver1_INVERTER"
 in "nexConf.h" enable debug and must be set to Serial2 ...
 Wiring the two data wires:            Wiring also the I2C protocol for SOC values from arduino NANO
-NEXTION Rx --> Tx2 MEGA               A4 NANO --> 20 MEGA
-NEXTION Tx --> Rx2 MEGA               A5 NANO --> 21 MEGA
+NEXTION Rx --> Tx2 MEGA (16)              A4 NANO --> 20 MEGA
+NEXTION Tx --> Rx2 MEGA (17)              A5 NANO --> 21 MEGA
 The input signals are switching icons on display - I used ProgressBAR for that trick!
-Make sure you import the ITEADlib library!!
+Make sure you import the ITEADlib library in arduino IDE!!
 The Relay module has to be connected to power a 10 seconds after the key power the Arduino & Nextion to prevent switching relays ON!
-input tester: 
-SWITCH DIP                           BUTTONS
-[1 ]  ???
-[2 ] 26 - parklight         [1] 30 - STOP
-[3 ] 28 - daylight          [2] 27 -  dooropen switch ok
-[4 ] 32 - long light        [3] 31 - right
-[5 ] 24 - fog light         [4] 25 - left 
-[6 ] 34 - frost             [1] 35 - HORN        open door switch
-[7 ] 36 - emergency         [2] ??  -  ???           RIGHT
-[8 ] 22 - handbrake
-]9 ] 37 - backward drive light
-[10] 23 - cabin switch light  ???
-30.8.2018 changed the obsolete SOC to DIM trimmer use for dimming the nextion display, 
-uses progress bar j13.
+Dimming the display with tuch function (Nextion code)
+Dimming the dash lights with potentimeter on dashboard SWITCH1
+11.9. 2018 fix the code (I/O pins) for MEga PRO CH340G. PINS from 2 ÷ 13 are PWM. 
 Author: Vasja Markič           
 */
 #include <Wire.h>
 #include <doxygen.h>
 //#include <NexButton.h>
 #include <NexConfig.h>
-//#include <NexCrop.h>
-//#include <NexGauge.h>
-//#include <NexHardware.h>
-//#include <NexHotspot.h>
-//#include <NexObject.h>
-//#include <NexPage.h>
+//#include <NexCrop.h> //#include <NexGauge.h> //#include <NexHardware.h> //#include <NexHotspot.h> //#include <NexObject.h> //#include <NexPage.h>
 #include <NexPicture.h>
 #include <NexProgressBar.h>
-//#include <NexSlider.h>
-//#include <NexText.h>
+//#include <NexSlider.h> //#include <NexText.h>
 #include <Nextion.h>
 #include <NexTouch.h>
 //#include <NexWaveform.h>
 #include "Nextion.h"
 
-// constants won't change. They're used here to set pin numbers, these are OUTPUTS:
-const int ParkLight =  53;           // na relay 1
-const int DayLight = 52;              // na relay 2
-const int LongLight = 51;               // na relay 3
-const int TurnLeft = 50;           // na relay 4 --> switching relay
-const int TurnRight = 49;          // na relay 5 --> switcging relay CF14 flasher unit
-const int StopLight = 48;            // na relay 6
-const int Horn = 47;                // na relay 7
-const int DefrostWindow = 42;      // relay 8 
-const int BackwardLight = 45;        // relay 9
-const int CabinLight = 44;         // relay 10
-const int FogLight = 43;           // relay 11
-const int EmergenceLight = 46;          // na relay 12
-const int WindowWater = 41;        // relay 13
-const int Wiper1Alternate = 38;     // relay 14 --> on auto wiper intermittent relay, timer
-const int Wiper1 = 39;         // relay 15 --> on auto wiper intermittent relay
-const int Wiper2 = 40;         // relay 16, direct to wiper motor
+// OUTPUTS pin declaration:
+const int ParkLight =  31;            // relay 1 - 2x front parklights, 2x back parklights, all dash lights
+const int DayLight = 30;              // relay 2 - 2x front daylights H4
+const int LongLight = 29;             // relay 3 - 2x front longlights H4
+const int TurnLeft = 28;              // relay 4 - 3x left turning lights (via swtching relay CF14 flasher unit)
+const int TurnRight = 27;             // relay 5 - 3x right turning lights (via switcging relay CF14 flasher unit)
+const int StopLight = 26;             // relay 6 - 2x back stoplights
+const int Horn = 25;                 // relay 7 - to horn acuator
+const int DefrostWindow = 24;        // relay 8 - to rear window defrost
+const int BackwardLight = 23;        // relay 9 - to rear backlight(s)
+const int WiperRear = 22;           // relay 10 - motorpump rear AND motor wiper rear
+// 21, 20 reserved for I2C comm with NANO soc-meter
+const int FogLight = 19;             // relay 11 - 2x front foglights
+const int EmergenceLight = 18;       // relay 12 --> to 6x external blinkers AND SWITCH3 (dash) (via CF14) - USE DIODE!
+// 17, 16 reserved for Tx2 and Rx2 comm with Nextion display
+const int WindowWater = 15;          // relay 13 - motorpump front
+const int Wiper1Alternate = 14;      // relay 14 - via auto wiper intermittent relay to motor wiper front, timer
+// 13 reserved integrated LED       // n.c.
+const int Wiper1 = 12;               // relay 15 - wire to auto wiper intermittent relay
+const int Wiper2 = 11;               // relay 16 - wire directly to wiper motor
+// PWM OUTUPTS (from 0 ÷ 13)
+const int DashLightPWM = 4;   // dimm dashboard lights
+const int TempGaugePWM = 3;    // drive temp. gauge
+const int FuelGaugePWM = 2;    // drive voltage pack (ex. fuel) gauge
 
 // INPUTS:
-const int ParkLight_Switch = 26;    // with contact ??
-const int DayLight_Switch = 28;      
-const int LongLight_Switch = 32;       
-const int TurnLeft_Switch = 25;   
-const int TurnRight_Switch = 31;  
-const int StopLight_switch = 30;    // switch on the brake foot
-const int EmergenceLight_Switch = 36; 
-const int Horn_Switch = 35;        // wheel drive button
-const int DefrostWindow_Switch = 34; 
-const int BackwardLight_Switch = 37;     // two-pole switch to change the drive direction!
-const int CabinLight_Switch = 23;  // switch for light the cabin
-const int DoorOpen_Switch = 27;   // micro switch on doors
-const int Handbrake_Switch = 22;       // hadnbrake microswitch
-const int FogLight_Switch = 24;   //
-// 31 REZERVA
-const int WindowWater_Switch = 12;
-const int Wiper1_Switch = 10;
-const int Wiper2_Switch = 11;
-const int Wiper1Alternate_Switch = 9;
-const int ChargeMode_input = 8;           // microswitch detects when AC plug cable is IN
+const int ParkLight_Switch = 53;      // SWITCH1 (on dash)
+const int DayLight_Switch = 52;       // SWITCH1 (on dash)
+const int LongLight_Switch = 51;      // WHEEL SWITCH1
+const int TurnLeft_Switch = 50;       // WHEEL SWITCH1
+const int TurnRight_Switch = 49;      // WHEEL SWITCH1
+const int StopLight_switch = 48;      // SWITCH2 (on the brake foot)
+const int EmergenceLight_Switch = 47;  //SWITCH3 (on dash)
+const int Horn_Switch = 46;            // WHEEL SWITCH?????
+const int DefrostWindow_Switch = 45;   //SWITCH4 (on dash)
+const int BackwardLight_Switch = 44;   // two-pole switch to change the drive direction! (on dash, added extra!)
+const int WiperRear_Switch = 43;      // SWITCH5 (undder the roof, near the light)
+const int DoorOpen_Switch = 42;        // MICROSWITCH1 (4 x doors)
+const int Handbrake_Switch = 41;       // MICROSWITCH2 (handbrake)
+const int FogLight_Switch = 40;        // SWITCH6 (extra added on dash)
+const int WindowWater_Switch = 39;     // WHEEL SWITCH2
+const int Wiper1_Switch = 38;          // WHEEL SWITCH2
+const int Wiper2_Switch = 37;          // WHEEL SWITCH2
+const int Wiper1Alternate_Switch = 36;    // WHEEL SWITCH2
+const int ChargeMode_input = 35;           // MICROSWITCH3 (when AC plug cable is IN, rezervoir tank)
+const int Reserve_input1 = 34;         // 
+// analoge INPUTS (from A0 ÷ A15)
 const int Battery12V_input = A0;          // analog input, resistor divider 1:15 ratio with capacitor
-const int DIM_input = A1;                 // analog input from BMS
+const int DIM_input = A1;                 // potentiometer on SWITCH1, for dimming the dash Lights
+const int BatteryPackVolt_input = A2;     // analog input, 86V ÷ 125V range via resistor divider R1 = 12K, R2 = 500E --> input range 3.44V ÷ 5V
+const int Temperature_input = A3;         // depends on the PTK probe!
 
 // ventilation via PWM power module
 
@@ -91,7 +83,6 @@ const int DIM_input = A1;                 // analog input from BMS
 // display temp. motor (0 ÷ 6 V)
 
 // variables will change - HIGH state relay OFF:
-bool sign_rocna_ON = true;
 bool ParkLight_Switch_ON = true;
 bool DayLight_Switch_ON = true;
 bool LongLight_Switch_ON = true;
@@ -104,21 +95,26 @@ bool DefrostWindow_Switch_ON = true;
 bool BackwardLight_Switch_ON = true;
 bool CabinLight_Switch_ON = true;
 bool DoorOpen_Switch_ON = true;
-bool Handbrake_Switch_ON =true;
+bool Handbrake_Switch_ON = true;
+bool WiperRear_Switch_ON = true;
 bool WindowWater_Button_ON = true;
 bool Wiper1Alternate_Switch_ON = true;
 bool Wiper1_Switch_ON = true;
 bool Wiper2_Switch_ON = true;
 bool FogLight_Switch_ON = true;
 bool ChargeMode_ON = false;       // if we plug the AC charging
-long DIM_Value = 0;      // initial value of the state of charge, from 0 to 1023  (measures 0 ÷ 5 V)
-long DIM;                        // voltage transformed to int from 0 ... 100
+long DIM_Value = 0;      // initial value of the dimming display, from 0 to 1023  (measures 0 ÷ 5 V)
+long DIM;                        // DIM transformed to int from 0 ... 100
 unsigned int Battery12V_Value = 0;    // initial value od digital value of the 12 V battery (0 ÷ 1023) 
+unsigned int BatteryPackVolt_Value = 0;
+unsigned int Temperature_Value = 0;
 float Battery12V;
+long BatteryPackVolt;      // transform 3.44V to 0 and 5.00V to 40
+long Temperature;         // transform 0.00V to 0 and 5.00V to 40
 unsigned int Battery12V_treshold = 11;   // set the min. voltage for signaling battery low voltage on display
 int wiper2_water = 0;
 char object[10];
-int SOC2;
+int SOC2;         //aquire from arduino NANO SOC Meter
 
 // Declaration of objects in NEXTION
 // NexProgressBar - progress bar --> value 100 (is pic1) or 0 (pic0)
@@ -159,20 +155,24 @@ void setup() {
 // OUTPUTS:
   pinMode(ParkLight, OUTPUT);          // relay 1
   pinMode(DayLight, OUTPUT);           // relay 2
-  pinMode(LongLight, OUTPUT);          // na relay 3
-  pinMode(TurnLeft, OUTPUT);           // na relay 4: --> 
-  pinMode(TurnRight, OUTPUT);          // na relay 5: -->
-  pinMode(StopLight, OUTPUT);          // na relay 6: 2
-  pinMode(Horn, OUTPUT);               // na relay 7: 1 x Horn
+  pinMode(LongLight, OUTPUT);          // relay 3
+  pinMode(TurnLeft, OUTPUT);           // relay 4
+  pinMode(TurnRight, OUTPUT);          // relay 5
+  pinMode(StopLight, OUTPUT);          // relay 6
+  pinMode(Horn, OUTPUT);               // relay 7: 1 x Horn
   pinMode(DefrostWindow, OUTPUT);       // relay 8
-  pinMode(BackwardLight, OUTPUT);       // relay 9: 1 
-  pinMode(CabinLight, OUTPUT);          // relay 10: more lights with optional progrmaming
+  pinMode(BackwardLight, OUTPUT);       // relay 9
+  pinMode(WiperRear, OUTPUT);          // relay 10: back wiper
   pinMode(FogLight, OUTPUT);            // relay 11
-  pinMode(EmergenceLight, OUTPUT);      // na relay 12 - to all flashes!
-  pinMode(WindowWater, OUTPUT);         // na relay 13 --> motor of water pump
+  pinMode(EmergenceLight, OUTPUT);      // relay 12 - to all flashes!
+  pinMode(WindowWater, OUTPUT);         // relay 13 motor of water pump front
   pinMode(Wiper1Alternate, OUTPUT);     // relay 14
   pinMode(Wiper1, OUTPUT);              // relay 15
   pinMode(Wiper2, OUTPUT);              // relay 16 
+  pinMode(DashLightPWM, OUTPUT);          // to MOSFET 1  drives dash lights via RELAY 1 (5 V), mosfet driven with 12 V
+  pinMode(TempGaugePWM, OUTPUT);          // to MOSFET 2  drives tempertature GAUGE (6 V), mosfet driven on 12 V
+  pinMode(FuelGaugePWM, OUTPUT);          // to MOSFET 3 drives fuel GAUGE (6 V), mosfet driven on 12 V
+  
 // INPUTS:
   pinMode(ParkLight_Switch, INPUT);
   pinMode(DayLight_Switch, INPUT);
@@ -183,7 +183,7 @@ void setup() {
   pinMode(Horn_Switch, INPUT);        // steering wheel button
   pinMode(DefrostWindow_Switch, INPUT); 
   pinMode(BackwardLight_Switch, INPUT);     // Two-poles switch 
-  pinMode(CabinLight_Switch, INPUT);  
+  pinMode(WiperRear_Switch, INPUT);  
   pinMode(EmergenceLight_Switch, INPUT);   
   pinMode(DoorOpen_Switch, INPUT); 
   pinMode(Handbrake_Switch, INPUT);
@@ -193,8 +193,10 @@ void setup() {
   pinMode(Wiper1_Switch, INPUT);  // speed 1
   pinMode(Wiper2_Switch, INPUT);  // speed 2 
   pinMode(ChargeMode_input, INPUT); // if the AC cable is pludged
-  pinMode(Battery12V_input, INPUT); // analog measurment of voltage 0 ÷ 12 V - wire a voltage divider with resistors (or Zener)!
-  pinMode(DIM_input, INPUT);    // dimming with trimmer, 0 - 5 V LCD brightness
+  pinMode(Battery12V_input, INPUT); // analog measurment of voltage 0 ÷ 12 V - wire a voltage divider with resistors
+  pinMode(DIM_input, INPUT);    // dimming with trimmer, 0 - 5V (1023) LCD brightness    (ANALOG)
+  pinMode(BatteryPackVolt_input, INPUT);   // range from 3.44V (703) to 5V (1023)   (ANALOG)
+  pinMode(Temperature_input, INPUT);    // from 0V (0) to 5V (1023) via PTK   (ANALOG)
 
 //main loop/program :
 void loop() {  
@@ -212,14 +214,12 @@ void readwrite() {
   TurnLeft_Switch_ON = digitalRead(TurnLeft_Switch);
   TurnRight_Switch_ON = digitalRead(TurnRight_Switch);
   StopLight_switch_ON = digitalRead(StopLight_switch);
-  EmergenceLight_Switch_ON = digitalRead(EmergenceLight_Switch);
-  SOC_Battery_Value = analogRead(SOC_input); // reading values from 0 - 5 V and converts to int value from 0 to 1023 .
-  Battery12V_Value = analogRead(Battery12V_input);
+  EmergenceLight_Switch_ON = digitalRead(EmergenceLight_Switch); 
   ChargeMode_ON = digitalRead(ChargeMode_input);
   Horn_Switch_ON = digitalRead(Horn_Switch);
   DefrostWindow_Switch_ON = digitalRead(DefrostWindow_Switch);
   BackwardLight_Switch_ON = digitalRead(BackwardLight_Switch);
-  CabinLight_Switch_ON = digitalRead(CabinLight_Switch);
+  WiperRear_Switch_ON = digitalRead(WiperRear_Switch);
   DoorOpen_Switch_ON = digitalRead(DoorOpen_Switch);
   Handbrake_Switch_ON = digitalRead(Handbrake_Switch);
   FogLight_Switch_ON = digitalRead(FogLight_Switch);
@@ -227,10 +227,23 @@ void readwrite() {
   Wiper1Alternate_Switch_ON = digitalRead(Wiper1Alternate_Switch);
   Wiper1_Switch_ON = digitalRead(Wiper1_Switch);
   Wiper2_Switch_ON = digitalRead(Wiper2_Switch);
+  DIM_Value = analogRead(DIM_input); // reading values from 0 - 5 V and converts to int value from 0 to 1023 .
+  Battery12V_Value = analogRead(Battery12V_input);
+  BatteryPackVolt_Value = analogRead(BatteryPackVolt_input);
+  Temperature_Value = analogRead(Temperature_input);
   
-
-// wiper program - OFF --> speed 1 alternating --> speed 1 --> speed 2. When watering the window, makes 1 alternate loop
- 
+  // 0 --> 0, 1023 --> 100;
+  DIM = DIM_Value*100/1023;
+  //equations for Fuel gauge and Temperature gauge:
+  // 703 --> 0, 1023 --> 100
+  BatteryPackVolt = (BatteryPackVolt_Value-703)*100/(1023-703);
+  // 0 --> 0, 1023 --> 100
+  Temperature = Temperature_Value*100/1023;
+  
+  // sending PWM singal to dash lights, temperature gauge and fuel gauge:
+  analogWrite(DashLightPWM, DIM);
+  analogWrite(FuelGaugePWM, BatteryPackVolt);
+  analogWrite(TempGaugePWM, Temperature);
 
 // when contact turns park lights on - relay 1
   if (ParkLight_Switch_ON == LOW) {
@@ -298,11 +311,11 @@ void readwrite() {
     digitalWrite(BackwardLight, HIGH);
     //j?.setValue(0);
   }
-// 3 position switch: 1. OFF, cabin light on when door open; 2. OFF always; 3. ON always - relay 10
-  if (CabinLight_Switch_ON == LOW or DoorOpen_Switch_ON == LOW) {
-    digitalWrite(CabinLight, HIGH);
+// Activation of rear wiper and watering pump
+  if (WiperRear_Switch_ON == LOW) {
+    digitalWrite(WiperRear, HIGH);
   } else {
-    digitalWrite(CabinLight, LOW);
+    digitalWrite(WiperRear, LOW);
   }
 // Fog lights on armature switch - relay 11
   if (FogLight_Switch_ON == LOW) {
@@ -369,13 +382,12 @@ void readwrite() {
   
 // dim the display - reading DIM_value, using potenciometer on A0
 // On nextion HMI file the page0 postinitialize event has code: j13.value=dim
-  DIM = DIM_Value*100/1023;
-  j13.setValue(DIM);
+  
+  j13.setValue(DIM);    //displaying the brigtness of the dash lights. 
 // Displaying SOC graphic via wire.h on NANO 
   j11.setValue(SOC2);    // when aquiring data from Arduino NANO, change to SOC2! 
  
-//  Serial.println(DIM);         // for diagnostic!
-//  Serial.println(DIM_Value);
+//  Serial.println(SOC2);         // for diagnostic!
 
 /* display of low 12v battery icon, if voltage is lower than 11 V reading from A11
   0 - ~17 V voltmeter
